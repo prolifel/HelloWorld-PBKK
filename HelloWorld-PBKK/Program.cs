ae934b2933330f3be6039dfcc11de124c856fa4e
Clement Prolifel Priyatama <clementprolifel@gmail.com>
2021-03-21 12:48:15 +0700

    Add project files.

---
+++ HelloWorld-PBKK/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld_PBKK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Nama saya Clement Prolifel Priyatama");
+            Console.WriteLine("NRP 05111840000013");
+            Console.WriteLine("Dari kelas Pemrograman Berbasis Kerangka Kerja (PBKK) B");
+        }
+    }
+}
